e586194824f17835ae34bce8c6b31ab153f8f221
Ajayi <JOSHUA BLESSING@DESKTOP-73KFCI4.com>
2024-05-02 19:05:12 +0100

    first

---
+++ 1-intro.js
@@ -0,0 +1,7 @@
+const number = 12
+if(number < 10){
+  console.log('small number');
+}else{
+  console.log('Large number')
+}
+console.log(`its my first node app!!!`)
---
+++ 4-names.js
@@ -0,0 +1,12 @@
+const secret =  'SUPER SECRET'
+const yes= require ('./5-utilis.js')
+
+const a = yes("Arike")
+const b =yes("Alonge")
+const c = yes("Temitope")
+//export default
+module.exports = {secret}
+
+console.log(a)
+console.log(b)
+console.log(c)
